38c51f7ad974b9f99e94929dfa9fad39d0f3324f
ultimatekaycee <ultimatekaycee@outlook.com>
2024-03-27 12:34:38 -0700

    few others

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Function to insers node as left-child of
+ *                           of another in binary tree.
+ * @parent: ptr to node to insert left-child.
+ * @value: value to store in new node.
+ *
+ * Return: If parent is NULL or error - NULL.
+ *         Otherwise - ptr to new node.
+ *
+ * Description: If parent has a left-child, new node
+ *              takes its place and old left-child is set as
+ *              left-child of new node.
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	another = binary_tree_node(parent, value);
+	if (another == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		another->left = parent->left;
+		parent->left->parent = another;
+	}
+	parent->left = another;
+
+	return (another);
+}
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Function to insert node as right-child
+ *                            of another in binary tree.
+ * @parent: ptr to node to insert right-child in.
+ * @value: value to store in new node.
+ *
+ * Return: If parent is NULL or error - NULL.
+ *         Otherwise - ptr to new node.
+ *
+ * Description: If parent has a right-child, new node
+ *              takes its place and old right-child is set as
+ *              right-child of new node.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	another = binary_tree_node(parent, value);
+	if (another == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		another->right = parent->right;
+		parent->right->parent = another;
+	}
+	parent->right = another;
+
+	return (another);
+}
